be1f4de47c1a23de5ebd219931e3616382376747
Ajith5566 <ajithbabu5566@gmail.com>
2024-08-18 00:40:32 +0530

    new code

---
+++ index.js
@@ -0,0 +1,45 @@
+document.querySelector('#navlogo button').addEventListener('click', function () {
+  document.querySelector('#navcomp').classList.toggle('show');
+  document.querySelector('#navlast').classList.toggle('show');
+});
+
+if (window.innerWidth <= 768) {
+  document.getElementById("content1").innerHTML = "category";
+}
+
+if (window.innerWidth <= 768) {
+  document.getElementById("content2").innerHTML = "Due date";
+}
+if (window.innerWidth <= 768) {
+  document.getElementById("content3").innerHTML = "Status";
+}
+if (window.innerWidth <= 768) {
+  document.getElementById("content4").innerHTML = "category";
+}
+
+if (window.innerWidth <= 768) {
+  document.getElementById("content5").innerHTML = "Due date";
+}
+if (window.innerWidth <= 768) {
+  document.getElementById("content6").innerHTML = "Status";
+}
+if (window.innerWidth <= 768) {
+  document.getElementById("content7").innerHTML = "category";
+}
+
+if (window.innerWidth <= 768) {
+  document.getElementById("content8").innerHTML = "Due date";
+}
+if (window.innerWidth <= 768) {
+  document.getElementById("content9").innerHTML = "Status";
+}
+if (window.innerWidth <= 768) {
+  document.getElementById("content10").innerHTML = "category";
+}
+
+if (window.innerWidth <= 768) {
+  document.getElementById("content11").innerHTML = "Due date";
+}
+if (window.innerWidth <= 768) {
+  document.getElementById("content12").innerHTML = "Status";
+}
